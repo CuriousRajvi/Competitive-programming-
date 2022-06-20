f7c9d8bd57e72898b71335c24153f717526eb6cd
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-06-20 16:14:56 +0530

    Create contains-duplicates-I.cpp

---
+++ Leetcode_Solutions/contains-duplicates-I.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+       map<int,int> count;
+       for(int i=0;i<nums.size();i++){
+           if(count[nums[i]]) return true;
+           count[nums[i]]++;
+        }
+        return false;
+    }
+};
