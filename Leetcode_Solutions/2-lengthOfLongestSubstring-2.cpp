300f45cc8f7a23ab8585ff3e936d6077ccbb9f30
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-07-25 16:19:13 +0530

    Create 2-lengthOfLongestSubstring-2.cpp

---
+++ Leetcode_Solutions/2-lengthOfLongestSubstring-2.cpp
@@ -0,0 +1,19 @@
+//Sliding Window
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int store[256]={0};
+        int l=0,r=0;
+        int ans=0;
+        while(r<s.length()){
+            store[s[r]]++;
+            while(store[s[r]]>1){
+                store[s[l]]--;
+                l++;
+            }
+            ans=max(ans,r-l+1);
+            r++;
+        }
+        return ans;
+    }
+};
