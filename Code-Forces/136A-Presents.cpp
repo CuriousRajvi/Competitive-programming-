72e6df2feb9a73580741224af3334d2f1c9ae077
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-07-29 17:17:21 +0530

    Create 136A-Presents.cpp

---
+++ Code-Forces/136A-Presents.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int b[n];
+    for(int i=1;i<=n;i++){
+        int a;
+        cin>>a;
+        b[a]=i;
+    }
+    for(int i=1;i<=n;i++){
+        cout<<b[i]<<" ";
+    }
+    return 0;
+}
