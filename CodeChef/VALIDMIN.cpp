018c42b028078984ea19a8a83cbeebe0a41da06e
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-08-01 17:46:10 +0530

    Create VALIDMIN.cpp

---
+++ CodeChef/VALIDMIN.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	   if(a==b && b==c)
+	        cout<<"YES"<<endl;
+	   else if(a==b && c>a)
+	       cout<<"YES"<<endl;
+	   else if(b==c && a>b)
+	       cout<<"YES"<<endl;
+	   else if(c==a && b>c)
+	       cout<<"YES"<<endl;
+	   else
+	        cout<<"NO"<<endl;
+	               
+	    
+	}
+	return 0;
+}
