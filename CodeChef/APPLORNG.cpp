347c6d587040f3ee6a7b2c2f490e7b54db884dd6
Rajvi Desai <66167509+CuriousRajvi@users.noreply.github.com>
2022-08-01 17:24:10 +0530

    Create APPLORNG.cpp

---
+++ CodeChef/APPLORNG.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	int a,b;
+	cin>>a>>b;
+	if(a+b<=n){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	return 0;
+}
